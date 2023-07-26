1b925458224354dfa9213a73d62ffb40c5944fea
Sandeep Ghosh <mailsg0912@gmail.com>
2023-07-26 20:23:42 +0530

    morse code solution

---
+++ morse.rb
@@ -0,0 +1,34 @@
+def decode_char(morse)
+  morse_code = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z'
+  }
+
+  morse_code[morse] || '' # Return corresponding character or empty string if not found
+end
+
+# Method to decode an entire word in Morse code
+def decode_word(word)
+  word.split.map { |morse| decode_char(morse) }.join
+end
+
+# Method to decode the entire message in Morse code
+def decode(message)
+  message.split('   ').map { |word| decode_word(word) }.join(' ')
+end
+
+puts decode_char('.-')
+# "A"
+
+puts decode_word('-- -.--')
+# "MY"
+
+puts decode('-- -.--   -. .- -- .')
+# "MY NAME"
+
+puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+# "A BOX FULL OF RUBIES"
